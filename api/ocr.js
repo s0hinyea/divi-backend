4e882be1514be18f01145a274c4fe91ffd04808e
Mohammed Islam <mtislam5757@gmail.com>
2025-04-30 14:05:59 -0400

    ocr testing with POST requests

--- api/ocr.js
+++ api/ocr.js
@@ -1,5 +1,6 @@
+import Tesseract from "tesseract.js";
 
-export default function handler(req, res){
+export default async function handler(req, res){
   if (req.method !== 'POST'){
     return res.status(405).json({ error: 'Only POST requests allowed'})
   };
@@ -9,11 +10,25 @@ export default function handler(req, res){
     return res.status(400) 
   }
 
+ try{
+  const base64Data = image.replace(/^data:image\/\w+;base64,/, "");
+  //takes image base64 string and strips off prefix so only actual Data Uri taken
+  const imageBuffer = Buffer.from(base64Data, 'base64')
+  //Tesseract reads raw binary data, not binary string, buffer turns it to that
+  //Buffer holds raw binary data in memory, lets tools treat the base64 as a file 
+
+  const result = await Tesseract.recognize(imageBuffer, 'eng');
+  const extractedText = result.data.text;
+
+
   return res.status(200).json({
-    status: 'received',
-    length: image.length,
-    snippet: image.slice(0, 30) + '...'
+    text: extractedText
   });
+} catch (error) {
+  console.error(" OCR ERROR", error);
+  return res.status(500).json({ error: 'Failed to process image'});
+}
+
 }
 
 // req.body example : {"image": "data:image/jpeg;base64,/9j/4AAQSkZ..."}
