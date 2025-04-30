497492aca3b1647977da931322d41f373368133d
Mohammed Islam <mtislam5757@gmail.com>
2025-04-30 12:57:32 -0400

    testing API

--- api/ocr.js
+++ api/ocr.js
@@ -1,4 +1,20 @@
 
 export default function handler(req, res){
-  res.status(200).json({ message: "OCR Working!"});
+  if (req.method !== 'POST'){
+    return res.status(405).json({ error: 'Only POST requests allowed'})
+  };
+
+  const { image } = req.body
+  if(!image) {
+    return res.status(400) 
+  }
+
+  return res.status(200).json({
+    status: 'received',
+    length: image.length,
+    snippet: image.slice(0, 30) + '...'
+  });
 }
+
+// req.body example : {"image": "data:image/jpeg;base64,/9j/4AAQSkZ..."}
+
